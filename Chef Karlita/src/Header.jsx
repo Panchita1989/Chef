633485140032402d9835ac16796e7fe9ea4d5cc5
Panchita <franciscalandwehr@hotmail.com>
2025-04-23 11:16:20 -0500

    open chefKarlita project

---
+++ Chef Karlita/src/Header.jsx
@@ -0,0 +1,10 @@
+import logo from '/src/assets/logo.png'
+
+export default function Header(){
+    return(
+        <header>
+            <img src={logo}/>
+            
+        </header>
+    )
+}
