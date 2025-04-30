eb98dc99f083f156e4e8b6a9f9097c4f355977bf
Panchita <franciscalandwehr@hotmail.com>
2025-04-29 23:01:37 -0500

    changed form

--- Chef Karlita/src/main.jsx
+++ Chef Karlita/src/main.jsx
@@ -24,7 +24,18 @@ export default function Main (){
             />
             <button>Add ingredient</button>
         </form>
-        <section>{ingredientsList}</section>
+        {ingredientsList.length > 0 &&
+        <section>
+            <h2>Ingredients on hand:</h2>
+            <ul className="ingredients-list" aria-live="polite">{ingredientsList}</ul>
+             {ingredientsList.length > 3 && <div className="get-recipe-container">
+                <div>
+                    <h3>Ready for a recipe?</h3>
+                    <p>Generate a recipe from your list of ingredients.</p>
+                </div>
+                <button>Get a recipe</button>
+            </div>}
+            </section>}
         </>
     )
 
