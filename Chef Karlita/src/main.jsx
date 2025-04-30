bed1592c3c1c88f552e24a33e91d852d788c7ea8
Panchita <franciscalandwehr@hotmail.com>
2025-04-30 13:39:01 -0500

    added Ingredients Component

--- Chef Karlita/src/main.jsx
+++ Chef Karlita/src/main.jsx
@@ -1,12 +1,9 @@
 import React from "react"
+import IngredientsList from "./components/ingredientsList"
 
 export default function Main (){
     const [ingredients, setIngredients] = React.useState([])
-    const ingredientsList = ingredients.map(element => (
-    <li key={element}>{element}</li>
-        ))
-    
-    
+       
     function handleSubmit(formData){
         const newIngredient = formData.get('ingredient')
         setIngredients(ingridient => [...ingridient, newIngredient])
@@ -14,7 +11,7 @@ export default function Main (){
     }
    
     return(
-        <>
+        <main>
         <form action={handleSubmit} className="add-ingredient-form">
             <input 
                 type="text"
@@ -24,19 +21,9 @@ export default function Main (){
             />
             <button>Add ingredient</button>
         </form>
-        {ingredientsList.length > 0 &&
-        <section>
-            <h2>Ingredients on hand:</h2>
-            <ul className="ingredients-list" aria-live="polite">{ingredientsList}</ul>
-             {ingredientsList.length > 3 && <div className="get-recipe-container">
-                <div>
-                    <h3>Ready for a recipe?</h3>
-                    <p>Generate a recipe from your list of ingredients.</p>
-                </div>
-                <button>Get a recipe</button>
-            </div>}
-            </section>}
-        </>
+        {ingredients.length > 0 && <IngredientsList ingredients = {ingredients} />
+        }
+        </main>
     )
 
     
